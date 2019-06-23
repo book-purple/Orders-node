191eb68cef97e90501b2c003f734a9188de99310
gaurav4ever <gauravsharma.mvp@gmail.com>
2019-06-23 17:02:14 +0530

    error response habndled

--- services/orderService.js
+++ services/orderService.js
@@ -8,7 +8,6 @@ const orderController = require("../controllers/orderController");
  */
 function createOrder(req, res) {
     console.log('create order service called...');
-    res.json("Create order service called");
     var order = new orderModel({
         order_id: 9001,
         created_at: new Date()
@@ -16,13 +15,15 @@ function createOrder(req, res) {
     console.log(JSON.stringify(order))
     order.save(function(err, result) {
         if (err) {
-            console.log("Some error occured", err);
+            res.json({
+                'errorMessage': 'unable to save order',
+                'error': err
+            });
         }
         if (result) {
-            console.log(result);
+            res.json(order);
         }
     });
-    //res.json(order);
 }
 
 module.exports.createOrder = createOrder;
